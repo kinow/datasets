4f513fd6a01c4e857bd340c234fc02d625e23b64
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-30 20:51:18 +1200

    GeoJSON events data

--- events/convert_events_2_geojson.py
+++ events/convert_events_2_geojson.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-from geojson import Point
+import geojson
 
 from pprint import pprint
 
@@ -9,9 +9,19 @@ with open('./events_chch_201608.json') as infile:
     with open('./events_chch_201608.geojson', 'w') as outfile:
         j = json.load(infile)
 
+        features = []
         for event in j:
             if event['point'] != None:
                 if event['point']['lat'] != None and event['point']['lng']:
-                    point = Point((float(event['point']['lat']), float(event['point']['lng'])))
-                    pprint(event)
-                    break
+                    # properties
+                    props = event.copy()
+                    del props['point']
+                    # geometry
+                    point = geojson.Point((float(event['point']['lng']), float(event['point']['lat'])))
+                    # and feature
+                    f = geojson.Feature(geometry=point, properties=props)
+                    features.append(f)
+
+        feature_col = geojson.FeatureCollection(features, crs="EPSG:4326")
+
+        json.dump(feature_col, outfile)
